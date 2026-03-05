9d2753e6fcc11fb7567053ad596c78c986fd7d42
Zellie Macabata <1995zellie@gmail.com>
2026-03-05 14:44:06 -0500

    remove docs, fix config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // config goes here
-  output: 'export',
+  output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-
-
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
